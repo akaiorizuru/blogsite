485db3c03966160a2b1c8dc3021c4a8b0a7763e4
akaiorizuru <60643510+akaiorizuru@users.noreply.github.com>
2021-11-29 13:50:20 +0100

    calcul variante couleur vert algo optimisé

---
+++ www/js/calcul.js
@@ -0,0 +1,47 @@
+let r=0;
+// let g=0; 
+let b=0;
+let all = 0;
+for (let g = 0; g <= 255; g++) {
+    while(r <= g){
+        r=r+1;
+        if(g !=r && g != b){
+            all = all + 1;
+        }
+        while(b <= g){
+            b = b + 1;
+            if(g !=r && g != b){
+                all = all + 1;
+            }
+        }
+    }
+    return all;
+}
+console.log(all);
+
+//version final
+// let all = 0;
+// for (let g = 0; g <= 255; g++) {
+//   for (let r = 0; r < g; r++) {
+//     for (let b = 0; b < g; b++) {
+//       all = all + 1;
+//     }
+//   }
+//   console.log(all)
+// }
+
+// let r = 0; 
+// let b = 0;
+// let g = 0;
+// let all = 0;
+// for (let g = 0; g <= 10; g++) {
+// // let green = g;
+//     for (let r = 0; r < g; r++) {
+//         for (let b = 0; b < g; b++) {
+//             all = all + 1;
+//             let rgb=[r,g,b]
+//             console.log(rgb);
+//         }
+//     }
+//     console.log(all)
+// }
--- www/js/test.js
+++ www/js/test.js
@@ -1,18 +1,29 @@
-const elements = Array.from(document.querySelectorAll('.fill'));
+// const elements = Array.from(document.querySelectorAll('.fill'));
 
-function fill(item, index) {
+// function fill(item, index) {
   
-  const bgColor = getComputedStyle(item).backgroundColor,
-  fillLayer = document.createElement('div');
-  fillLayer.classList.add('fill-layer');
-  item.style.backgroundColor = 'transparent';
-  item.style.position = 'relative';
-  item.style.overflow = 'hidden';
-  setTimeout(function() {
-    fillLayer.style.backgroundColor = bgColor;  
-    item.appendChild(fillLayer);
-  }, index * 1000);
+//   const bgColor = getComputedStyle(item).backgroundColor,
+//   fillLayer = document.createElement('div');
+//   fillLayer.classList.add('fill-layer');
+//   item.style.backgroundColor = 'transparent';
+//   item.style.position = 'relative';
+//   item.style.overflow = 'hidden';
+//   setTimeout(function() {
+//     fillLayer.style.backgroundColor = bgColor;  
+//     item.appendChild(fillLayer);
+//   }, index * 1000);
   
+// }
+
+// elements.forEach(fill);
+
+
+let all = 0;
+for (let g = 0; g <= 255; g++) {
+  for (let r = 0; r < g; r++) {
+    for (let b = 0; b < g; b++) {
+      all = all + 1;
+    }
+  }
+  console.log(all)
 }
-
-elements.forEach(fill);
